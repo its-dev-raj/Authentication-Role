3ce2d462d39eb63988421c2653ebcd4e393ffd0f
Dageshwar07 <dageshwardasmanikpuri996@gmail.com>
2025-05-22 18:30:16 +0530

    env file location changes

--- client/src/servies/api.js
+++ client/src/servies/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
-
 const local = import.meta.env.VITE_APP_LOCALBACKEND;
-console.log("the backend is on =", import.meta.env.VITE_APP_LOCALBACKEND);
-
+console.log("the backend is on =", local);
 const api = axios.create({
   baseURL: `${local}/api`,
   headers: {
